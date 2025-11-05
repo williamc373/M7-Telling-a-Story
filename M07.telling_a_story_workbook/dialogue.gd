27df4c0a69e12ce61836f64d78ef2ad206ac9e67
williamc373 <wicrowell@s.sfusd.edu>
2025-11-04 17:19:30 -0800

    M7L6 complete

--- M07.telling_a_story_workbook/dialogue.gd
+++ M07.telling_a_story_workbook/dialogue.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 
 var dialogue_items: Array [String] = [
 	"roses are red",
@@ -13,7 +14,17 @@ var current_item_index := 0
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item
-	rich_text_label.text = current_item
+	rich_text_label.visible_ratio = 0.0
+
+	var tween := create_tween()
+	var text_appearing_duration :=1.2
+	tween.tween_property(rich_text_label,"visible_ratio",1.0,text_appearing_duration)
+	
+	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_start_position := randf() * sound_max_offset
+	audio_stream_player.play(sound_start_position)
+	tween.finished.connect(audio_stream_player.stop)
+
 	
 func _ready() -> void:
 	show_text()
