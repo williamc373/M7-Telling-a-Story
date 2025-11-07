6b6f90594fa45189e5f486365f60ab0f736540a6
williamc373 <wicrowell@s.sfusd.edu>
2025-11-07 11:52:14 -0800

    M7L8 complete

--- M07.telling_a_story_workbook/expression.gd
+++ M07.telling_a_story_workbook/expression.gd
@@ -6,3 +6,32 @@ extends Control
 @onready var button_pink: Button = %ButtonPink
 @onready var button_regular: Button = %ButtonRegular
 @onready var button_happy: Button = %ButtonHappy
+@onready var button_sad: Button = %ButtonSad
+var bodies := {
+	"sophia" : preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png")
+}
+var expressions := {
+	"happy": preload("res://assets/emotion_happy.png"),
+	"regular": preload("res://assets/emotion_regular.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+}
+func _ready()-> void:
+	body.texture= bodies["pink"]
+	expression.texture = expressions["happy"]
+	
+	button_sophia.pressed.connect(func() -> void:
+		body.texture = bodies["sophia"]
+	)
+	button_pink.pressed.connect(func() -> void:
+		body.texture = bodies["pink"]
+	)
+	button_regular.pressed.connect(func() -> void:
+		expression.texture = expressions["regular"]
+	)
+	button_sad.pressed.connect(func() -> void:
+		expression.texture = expressions["sad"]
+	)
+	button_happy.pressed.connect(func() -> void:
+		expression.texture = expressions["happy"]
+	)
